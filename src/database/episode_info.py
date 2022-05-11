806a1cc798e7a762c29683b588fca023d7e7c0fb
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2022-05-11 22:17:59 +0200

    [DB] - Add function to manage some project's table
    [+] function to init episode table 
    
    [+]  function to test if an episode is in the db or not
    
    [+]  function to insert a new episode in the db
    
    [*] Improve documentation
    
    [+] new init section : port
         ---- [C]  Very important for psycopg2 librairie

--- src/database/episode_info.py
+++ src/database/episode_info.py
@@ -1,6 +1,26 @@
 
-# SQL command
+#python librairie for log
+from configparser import ConfigParser
+from python_tracer.Logger import VerboseLevel,Logger
 
+from datetime import datetime
+from psycopg2 import connect, Error
+
+config = ConfigParser()
+config.read("nariko.ini")
+log_level       = int(config.get("log", "prod_env"))
+log_path        = config.get("log", "path")
+extension       = config.get("log", "extension")
+
+username = config.get("database", "name")
+password = config.get("database", "password")
+host     = config.get("host_info", "host")
+port     = config.get("host_info", "port")
+db_name  = config.get("database", "db_name")
+
+log = Logger(log_path,log_level,service_name="name-extractor", log_extension=extension)
+
+# SQL command
 episode_psql_creation = '''CREATE TABLE episode_info (
                                 id SERIAL NOT NULL,
                                 anime_name VARCHAR(100) NOT NULL,
@@ -10,6 +30,12 @@ episode_psql_creation = '''CREATE TABLE episode_info (
                                 hash VARCHAR(42) NOT NULL,
                                 done_at INT NULL,
                                 PRIMARY KEY (id));'''
+
+insert_new_episode_psql = '''INSERT INTO 
+                                episode_info ( anime_name, saison, episode, clip_duration, hash, done_at ) 
+                                VALUES ( %(name)s, %(saison)s, %(episode)s, %(clip_duration)s, %(hash)s, %(done_at)s );'''
+
+is_in_db_psql = ''' SELECT id FROM episode_info WHERE anime_name=%(name)s AND saison=%(saison)s AND episode=%(episode)s ;'''
 
 def start_connexion():
     """
@@ -50,16 +76,105 @@ def end_connexion(con, cur):
         return False
 
 def episode_table_creation():
+    """
+        Create the episode table
+            Parameters:
+                None
+
+            Returns:
+                _ (bool): is table succesfully created
+    """
     _ = start_connexion()
-    if _["result"]:
-        log.info("episode's table creation")
-        connexion, cursor = _["info"]
-        try :
-            cursor.execute(episode_psql_creation)
-            connexion.commit()
-            log.done("table created ")
-            return True
-        except (Exception, Error) as error:
-            log.error(error)
-            nb_error = nb_error +1
-            return False
+    if not _["result"]:
+        log.error(_["info"])
+        return False
+    
+    log.info("episode's table creation")
+    connexion, cursor = _["info"]
+    try :
+        cursor.execute(episode_psql_creation)
+        connexion.commit()
+        end_connexion(connexion, cursor)
+        log.done("table created ")
+        return True
+    except (Exception, Error) as error:
+        log.error(str(error).replace('\n', ' '))
+        return False
+    
+def insert_new_episode(name:str,saison:int,episode:int, duration:int, _hash:str):
+    """
+        Insert a new episode into the episode database
+            Parameters:
+                name    (str): name of the anime
+                saison  (int): saison number
+                episode (int): episode number
+                duration(int): duration of one clip
+                hash    (str): the episode hash
+            
+            Return:
+                is_ok  (bool): is insert successfull or not
+                r_code  (int): how the function ended
+    """
+    if not (type(name) == str and type(saison) == int and type(episode) == int and type(duration)== int and type(_hash) == str) :
+        log.warning("name : %s, saison : %s, episode : %s" % (type(name),type(saison),type(episode)))
+        log.warning("clip duration : %s, hash : %s" % (type(duration), type(_hash)) )
+        log.error("One of the parameter's type is wrong.")
+        return False, 532
+    is_ok, return_code = is_in_db(name,saison, episode)
+    if return_code > 300:
+        return False, 533
+    _ = start_connexion()
+    if not _["result"]:
+        log.error(_["info"])
+
+    log.info("Insert new episode  : %(hash)s" % {'hash': _hash})
+    connexion, cursor = _["info"]
+
+    try :
+        done_at = datetime.timestamp(datetime.now())
+        cursor.execute(is_in_db_psql, {'name':name,'saison' : saison, 'episode': episode, 'clip_duration' : duration, 'hash' : _hash, 'done_at' : done_at})
+        connexion.commit()
+        end_connexion(connexion, cursor)
+        return True, 200
+    
+    except (Exception, Error) as error:
+        log.error(error)
+        return False, 400  
+
+def is_in_db(name:str,saison:int, episode:int):
+    """
+        Insert a new episode into the episode database
+            Parameters:
+                name    (str): name of the anime
+                saison  (int): saison number
+                episode (int): episode number
+            
+            Return:
+                is_ok  (bool): is insert successfull or not
+                r_code  (int): how the function ended
+
+    """
+    if not (type(name) == str and type(saison) == int and type(episode) == int) :
+        log.warning("name : %s, saison : %s, episode : %s" % (type(name),type(saison),type(episode)))
+        log.error("One of the parameter's type is wrong.")
+        return False, 532
+
+    _ = start_connexion()
+    if not _["result"]:
+        log.error(_["info"])
+        return False, 532
+    
+    log.info("Searching for episode  : %(hash)s" % {'hash':hash(name+str(saison)+str(episode))})
+    connexion, cursor = _["info"]
+
+    try :
+        cursor.execute(is_in_db_psql, {'name':name,'saison' : saison, 'episode': episode})
+        is_in_db = cursor.fetchall() == None
+        
+        end_connexion(connexion, cursor)
+        return is_in_db, 200
+    
+    except (Exception, Error) as error:
+        log.error(error)
+        return False, 400
+
